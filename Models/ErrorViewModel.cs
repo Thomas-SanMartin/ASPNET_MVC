ebf86da329d1990146a7a1d85ec41bcea1ae1d66
Thomas-SanMartin <123770648+Thomas-SanMartin@users.noreply.github.com>
2024-12-26 20:21:35 -0700

    Initial project setup

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AspNetMvcAssignment.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
